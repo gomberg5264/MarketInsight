24badb6c29981e9c041053373943cb2434bbb8cb
idealwebsolutions <alex@idealwebsolutions.co>
2021-04-11 23:59:04 -0700

    add REDIS_USERNAME to pm2 production configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,6 +18,7 @@ module.exports = {
       API_TOKEN: process.env.API_TOKEN,
       REDIS_HOST: process.env.REDIS_HOST,
       REDIS_PORT: process.env.REDIS_PORT,
+      REDIS_USERNAME: process.env.REDIS_USERNAME,
       REDIS_PASSWORD: process.env.REDIS_PASSWORD
     }
   }],
